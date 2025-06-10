c54f603eb5ae3ee67d25cc9b035b0d99db8d6c6a
Jacopo Cirica <91603474+JacopoCirica@users.noreply.github.com>
2025-06-10 15:02:31 -0700

    Update layout.tsx 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'HABIT',
+  title: 'HABIT Experiment',
   description: 'Created by Jacopo',
   generator: 'v0.dev',
 }
